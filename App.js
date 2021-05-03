51af489da118d53bffd3e7d7f8e67dd8da993b96
huybinh0204 <huybinh020403@gmail.com>
2021-05-03 11:55:06 +0700

    Ghi chú Commit

--- App.js
+++ App.js
@@ -1,21 +1,101 @@
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Alert, Button, Platform, Text, View } from 'react-native'
+import RNWhatsAppStickers from 'react-native-whatsapp-stickers'
+
+const config = {
+  identifier: 'myprojectstickers',
+  name: 'MyProject Stickers',
+  publisher: 'John Doe',
+  trayImageFileName: 'tray_Cuppy.png',
+  publisherEmail: 'xyz@myproject.xy',
+  publisherWebsite: 'https://github.com/Jobeso/react-native-whatsapp-stickers',
+  privacyPolicyWebsite:
+      'https://github.com/Jobeso/react-native-whatsapp-stickers',
+  licenseAgreementWebsite:
+      'https://github.com/Jobeso/react-native-whatsapp-stickers/blob/master/LICENSE',
+  stickers: [
+    {
+      fileName: '01_Cuppy_smile.png',
+      emojis: ['☕', '🙂'],
+    },
+    {
+      fileName: '02_Cuppy_lol.png',
+      emojis: ['😄', '😀'],
+    },
+    {
+      fileName: '03_Cuppy_rofl.png',
+      emojis: ['😆', '😂'],
+    },
+  ],
+}
+
+const { stickers, ...packConfig } = config
+
+const logError = e => {
+  console.log(e)
+  Alert.alert('Error', e.message)
+}
+
+const sendStickerPack = () => {
+  if (Platform.OS === 'ios') {
+    // return RNWhatsAppStickers.createStickerPack(packConfig)
+    //     .then(() => {
+    //       const promises = stickers.map(item =>
+    //           RNWhatsAppStickers.addSticker(item.fileName, item.emojis)
+    //       )
+    //       Promise.all(promises).then(() => RNWhatsAppStickers.send())
+    //     })
+    //     .catch(e => console.log(e))
+    RNWhatsAppStickers.createStickerPack(packConfig)
+        .then(() => {
+          const promises = stickers.map(item =>
+              RNWhatsAppStickers.addSticker(item.fileName, item.emojis)
+          )
+          Promise.all(promises).then(() => RNWhatsAppStickers.send())
+        })
+        .catch(logError)
+  } else {
+
+    RNWhatsAppStickers.send('myprojectstickers', 'MyProject Stickers')
+        .then(() => console.log('success'))
+        .catch(
+            logError
+        )
+  }
+}
+
+const TextBold = ({ children }) => (
+    <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{children}</Text>
+)
+
 const App = () => {
+  const [isAvailable, setIsAvailable] = useState(false)
+
+  useEffect(() => {
+    RNWhatsAppStickers.isWhatsAppAvailable()
+        .then(setIsAvailable)
+        .catch(console.log("222")
+    // logError
+  )
+  }, [])
+
   return (
-        <View style={{marginTop:30,flex:1}}>
-          <Text>2</Text>
-        </View>
-  );
-};
-
-
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 28, fontWeight: 'bold', marginBottom: 48 }}>
+          RNWhatsAppStickers
+        </Text>
+        <Text style={{ marginBottom: 24, fontSize: 16 }}>
+          WhatsApp is{' '}
+          {isAvailable ? (
+              <TextBold>available</TextBold>
+          ) : (
+              <TextBold>not available</TextBold>
+          )}
+        </Text>
+        {isAvailable && (
+            <Button title="Send Stickers" onPress={sendStickerPack} />
+        )}
+      </View>
+  )
+}
 export default App;
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  AppStickers
+//
+//  Created by Admin on 5/3/21.
+//
+
+import Foundation
