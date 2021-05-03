500892a40e0841736ebb87d121d8c961b228e8ea
huybinh0204 <huybinh020403@gmail.com>
2021-05-03 10:38:42 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+const App = () => {
+  return (
+        <View style={{marginTop:30,flex:1}}>
+          <Text>2</Text>
+        </View>
+  );
+};
+
+
+export default App;
